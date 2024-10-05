331faecc4652a0a486d3dd8c8f78ed8f7cae2194
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-10-05 22:02:13 +0545

    feat: adding dashboard form

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className='flex justify-center items-center min-h-screen pb-20'>
+    <div className='flex justify-center items-center min-h-[calc(100vh-7.5rem)] pb-20'>
       {children}
     </div>
   );
--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,3 +1,72 @@
+'use client';
+
+import { useUser } from '@clerk/nextjs';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { usernameSchema } from '@/lib/validators';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
 export default function DashboardPage() {
-  return <div>DashboardPage</div>;
+  const { user } = useUser();
+
+  const form = useForm({
+    resolver: zodResolver(usernameSchema),
+    defaultValues: {
+      username: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof usernameSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div className='space-y-8'>
+      <Card>
+        <CardHeader>
+          <CardTitle>Welcome, {user?.firstName}!</CardTitle>
+        </CardHeader>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Your Unique Link</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+              <FormField
+                control={form.control}
+                name='username'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <div className='flex items-center gap-2'>
+                        <span>{window?.location.origin}/</span>
+                        <Input placeholder='username' {...field} />
+                      </div>
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type='submit'>Update Username</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
